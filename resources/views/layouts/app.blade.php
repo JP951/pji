e6e26a79c644bc4626ad983ab9c382291e8cf8df
Paulo <paulo1100@gamil.com>
2024-11-06 21:15:19 -0300

    aula2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width= , initial-scale=1.0">
-    <title>Document</title>
+    <title>{{config('app.name')}} - @yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    @livewireStyles
 </head>
 <body>
+
+     @include('includes.header');
+
+     <div class="container">
+      @yield(section: 'content');
+     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-     <h1>Aula</h1>
+@livewireScripts     
+<h1>Aula</h1>
+
 </body>
 </html>
